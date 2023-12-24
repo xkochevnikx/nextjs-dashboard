39b8f8fdbeaf5e9087e787c50491a6effca8da73
xkochevnikx <xkochevnikx@protonmail.com>
2023-12-24 12:43:55 +0300

    fix

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,21 +12,13 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  // добавление noStore() предотвращает кеширование ответа.
+  // это эквивалентно добавлению второго параметра в фетч fetch(..., {cache: 'no-store'}).
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
     noStore();
-
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
